429742a36e0b019b8fd6b11e63b0015b4f452cc5
hlibbc <hlib.master@gmail.com>
2025-05-17 22:41:52 +0900

    monorepo: Configuring a single project to have multiple main (codeup)

---
+++ projects/codeup/codeup/basic049.py
@@ -0,0 +1,7 @@
+# 정수 2개 입력받아 비교하기 (>=, <=)
+def main():
+    a, b = input().split()
+    print(int(a) == int(b))
+
+if __name__ == "__main__":
+    main()
---
+++ projects/codeup/codeup/basic050.py
@@ -0,0 +1,7 @@
+# 정수 2개 입력받아 비교하기 (>=, <=)
+def main():
+    a, b = input().split()
+    print(int(b) >= int(a))
+
+if __name__ == "__main__":
+    main()
